101ce7e63afcd49b863e4f1250fdbd6fe1b5eb1a
hoanganhit1997 <hoanganhit1997@gmail.com>
2017-05-11 20:34:39 +0700

    Cap nhap file Route va Welcomcontroller

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,5 +1,9 @@
 <?php namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
 class WelcomeController extends Controller {
 
 	public function toMeo()
