bbe4539706d78ac92cdeea1f94222eaa4dadfc65
conghieutlvy <conghieutlvy@gmail.com>
2017-05-11 20:08:57 +0700

    cập nhật route & DoExam

---
+++ app/Http/Controllers/DoExamController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+class DoExamController extends Controller {
+		public function doexam(){}
+}
