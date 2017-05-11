7a8e0cdc40baa4291325109d7e5702fdb0f85217
conghieutlvy <conghieutlvy@gmail.com>
2017-05-11 20:59:58 +0700

    Cập nhật hàm hiển thị câu hỏi

--- resources/views/doExamPage.blade.php
+++ resources/views/doExamPage.blade.php
@@ -61,3 +61,85 @@
 @section('ans9')
 	<?php getanswer(9, $row, $url);?>
 @stop
+<?php 
+function getanswer($number,$row, $url){	
+		foreach($row as $key => $val){
+			if($key == "question"){?>
+			<div class="row">
+                            <div class="col-xs-1">
+                            </div>
+                            <div class="col-xs-10">
+                                <h3>
+								Câu <?php echo (($number+1).":  ".$val) ?>
+								</h3>
+                            </div>
+                            <div class="col-xs-1">
+                            </div>
+                    </div>
+                    <br /><br />
+		<?php }
+			if($key == "img_src") { ?>
+            <center><img src="
+		<?php echo $url.$val; ?>
+        	" alt = ""/></center>
+		<?php }
+			if ($key == "c0"){?>
+            <div class="row">
+                <div class="col-xs-2">
+                </div>
+				<div class="col-xs-8">
+                    <input onclick="active_num(<?php echo $number."0";?>);" type="checkbox" name="cb[]" value="<?php echo $number."0";?>" /> 
+                          <?php echo ($val) ?>
+                                 
+                </div>
+                <div class="col-xs-2">
+                </div>
+            </div>
+            <br /><br />
+            <?php 
+			}
+			if ($key == "c1"){?>
+            		<div class="row">
+                            <div class="col-xs-2">
+                            </div>
+							<div class="col-xs-8">
+                                 <input onclick="active_num(<?php echo $number."1";?>);" type="checkbox" name="cb[]" value="<?php echo $number."1";?>" /> 
+                                 <?php echo ($val) ?>
+                                 
+                            </div>
+                            <div class="col-xs-2">
+                            </div>
+                         </div><br /><br />
+            <?php 
+			}
+			if ($key == "c2" && $val != NULL){?>
+            		<div class="row">
+                            <div class="col-xs-2">
+                            </div>
+							<div class="col-xs-8">
+                                 <input onclick="active_num(<?php echo $number."2";?>);" type="checkbox" name="cb[]" value="<?php echo $number."2";?>" /> 
+                                 <?php echo ($val) ?>
+                                 
+                            </div>
+                            <div class="col-xs-2">
+                            </div>
+                         </div><br /><br />
+            <?php 
+			}
+			if ($key == "c3" && $val != NULL){?>
+            		<div class="row">
+                            <div class="col-xs-2">
+                            </div>
+							<div class="col-xs-8">
+                                 <input onclick="active_num(<?php echo $number."3";?>);" type="checkbox" name="cb[]" value="<?php echo $number."3";?>" /> 
+                                 <?php echo ($val) ?>
+                                 
+                            </div>
+                            <div class="col-xs-2">
+                            </div>
+                         </div><br /><br />
+            <?php 
+			}
+		}
+}
+	?>
