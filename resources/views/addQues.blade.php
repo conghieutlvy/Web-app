36a6eb5fbfa58c5aa19de716dd6076fb3638f9bc
conghieutlvy <conghieutlvy@gmail.com>
2017-05-13 00:12:41 +0700

    Cập nhật kiểm tra form

--- resources/views/addQues.blade.php
+++ resources/views/addQues.blade.php
@@ -25,7 +25,7 @@
                                 <input id="answer1" type="second-answer" class="form-control" name="answer1" required>
                             </div>
 							<div class="col-md-2">
-								<input id = "cb1" type="checkbox" name="a1" value="1" /> 
+								<input id = "cb1" onclick="switchadd();" type="checkbox" name="a1" value="1" /> 
 							</div>
                         </div>
 
@@ -36,7 +36,7 @@
                                 <input id="answer2" type="second-answer" class="form-control" name="answer2" required>
                             </div>
 							<div class="col-md-2">
-								<input id = "cb2" type="checkbox" name="a2" value="1" /> 
+								<input id = "cb2" onclick="switchadd();" type="checkbox" name="a2" value="1" /> 
 							</div>
                         </div>
 
@@ -47,7 +47,7 @@
                                 <input id="answer3" type="third-answer" class="form-control" name="answer3">
                             </div>
 							<div class="col-md-2">
-								<input id = "cb3" type="checkbox" name="a3" value="1" /> 
+								<input id = "cb3" onclick="switchadd();" type="checkbox" name="a3" value="1" /> 
 							</div>
                         </div>
 						
@@ -58,13 +58,13 @@
                                 <input id="answer4" type="fourth-answer" class="form-control" name="answer4">
                             </div>
 							<div class="col-md-2">
-								<input id = "cb4" type="checkbox" name="a4" value="1" /> 
+								<input id = "cb4" onclick="switchadd();" type="checkbox" name="a4" value="1" /> 
 							</div>
                         </div>
 						
 						<div class="form-group">
                             <center>
-								<button onclick="vailidation();" id="btadd" type="submit" class="btn btn-primary">
+								<button disabled="disabled" id="btadd" type="submit" class="btn btn-primary">
                                     Thêm
 								</button>
 							</center>
@@ -77,29 +77,24 @@
 </div>
 
 <script language="javascript">
-	function vailidation() {
-		document.getElementById("ques").innerHTML = "aa";
-		var cb3,cb4;
+	function switchadd() {
+		var answer1 = document.getElementById('answer1');
+		var answer2 = document.getElementById('answer2');
 		var answer3 = document.getElementById('answer3');
 		var answer4 = document.getElementById('answer4');
-		if(answer3 != '') cb3 = document.getElementById('cb3');
-		if(answer4 != '') cb4 = document.getElementById('cb4');
 		var cb1 = document.getElementById('cb1');
 		var cb2 = document.getElementById('cb2');
-		if(!(cb1.checked || cb2.checked || cb3.checked ||cb4.checked)){
-			alert("Vui lòng chọn ít nhất 1 đáp án đúng bằng cách tích vào ô tương ứng");
-			document.getElementById('answer1').setAttribute("value","{{ old('answer1') }}");
-			document.getElementById('answer2').setAttribute("value","{{ old('answer2') }}");
-			answer3.setAttribute("value","{{ old('answer3') }}");
-			answer4.setAttribute("value","{{ old('answer4') }}");
+		var cb3 = document.getElementById('cb3');
+		var cb4 = document.getElementById('cb4');
+		if((cb1.checked && answer1 != null)
+		 ||(cb2.checked && answer2 != null)
+		 ||(cb3.checked && answer3 != null)
+		 ||(cb4.checked && answer4 != null)){
+			document.getElementById("btadd").removeAttribute("disabled");
 		}
 		else{
-			document.getElementById('answer1').removeAttribute("value");
-			document.getElementById('answer2').removeAttribute("value");
-			answer3.removeAttribute("value");
-			answer4.removeAttribute("value");
+			document.getElementById("btadd").setAttribute("disabled","disabled");
 		}
 	}
-	
 </script>
 @endsection
