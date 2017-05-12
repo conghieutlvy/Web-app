58e5a31f0abaddec3b974f854a8585b5dbc1bc3c
conghieutlvy <conghieutlvy@gmail.com>
2017-05-12 23:51:11 +0700

    Cập nhật trang câu hỏi

--- resources/views/cauHoi.blade.php
+++ resources/views/cauHoi.blade.php
@@ -2,94 +2,64 @@
 @extends('Template/header')
 
 @section('page')
-
+<br /><br />
+<center><h2> Danh sách câu hỏi có trong đề thi </h2></center>
+<br /><br />
     <?php
-        $url = '../picture/';
-    foreach($data as $data1) {
-        foreach ($data1 as $row) {
-             getanswer(0, $row, $url);        
+     $url = '../picture/';
+	$i = 0;
+    foreach($data as $ques) {
+             getanswer($i, $ques, $url); 
+			 $i++;       
         }
-    }
-function getanswer($number,$row, $url){ 
-        foreach($row as $key => $val){
-            if($key == "question"){?>
+function getanswer($number,$ques, $url){ 
+            ?>
             <div class="row">
-                            <div class="col-xs-1">
+                            <div class="col-xs-2">
                             </div>
-                            <div class="col-xs-10">
+                            <div class="col-xs-9">
                                 <h3>
-                                Câu <?php echo (($number+1).":  ".$val) ?>
+                Câu <?php echo (($number+1).":  ".$ques['question']) ?>
                                 </h3>
                             </div>
                             <div class="col-xs-1">
                             </div>
                     </div>
-                    <br /><br />
-        <?php }
-            if($key == "img_src") { ?>
+                    <br />
+        <?php if(isset($ques["name_img"])) { ?>
             <center><img src="
-        <?php echo $url.$val; ?>
+        <?php echo $url.$ques["name_img"]; ?>
             " alt = ""/>
             </center>
         <?php }
-            if ($key == "c0"){?>
+		
+            if($ques["a0"] == 1)
+				initques($ques["c0"]);
+			if($ques["a1"] == 1)
+				initques($ques["c1"]);
+			if($ques["a2"] == 1)
+				initques($ques["c2"]);
+			if($ques["a3"] == 1)
+				initques($ques["c3"]);
+				?>
+            <br />
+            <?php           
+}
+	function initques($val){
+		?>
             <div class="row">
-                <div class="col-xs-2">
+                <div class="col-xs-3">
                 </div>
-                <div class="col-xs-8">
-                    <input type="checkbox" name="cb[]" value="<?php echo $number."0";?>" /> 
+                <div class="col-xs-7">
+                    <input type="checkbox" checked="checked" disabled="disabled" /> 
                           <?php echo ($val) ?>
                                  
                 </div>
                 <div class="col-xs-2">
                 </div>
             </div>
-            <br /><br />
-            <?php 
-            }
-            if ($key == "c1"){?>
-                    <div class="row">
-                            <div class="col-xs-2">
-                            </div>
-                            <div class="col-xs-8">
-                                 <input  type="checkbox" /> 
-                                 <?php echo ($val) ?>
-                                 
-                            </div>
-                            <div class="col-xs-2">
-                            </div>
-                         </div><br /><br />
-            <?php 
-            }
-            if ($key == "c2" && $val != NULL){?>
-                    <div class="row">
-                            <div class="col-xs-2">
-                            </div>
-                            <div class="col-xs-8">
-                                 <input type="checkbox"  /> 
-                                 <?php echo ($val) ?>
-                                 
-                            </div>
-                            <div class="col-xs-2">
-                            </div>
-                         </div><br /><br />
-            <?php 
-            }
-            if ($key == "c3" && $val != NULL){?>
-                    <div class="row">
-                            <div class="col-xs-2">
-                            </div>
-                            <div class="col-xs-8">
-                                 <input type="checkbox"  /> 
-                                 <?php echo ($val) ?>
-                                 
-                            </div>
-                            <div class="col-xs-2">
-                            </div>
-                         </div><br /><br />
-            <?php 
-            }
-        }
-}
+            <br />
+		<?php	
+	}
 ?>
 @stop            
