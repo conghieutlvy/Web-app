076040aae960f6d54d57a1ee0394998597d5ba8c
hoanganhit1997 <hoanganhit1997@gmail.com>
2017-05-12 15:46:43 +0700

    Sua addQues

--- resources/views/addQues.blade.php
+++ resources/views/addQues.blade.php
@@ -64,11 +64,9 @@
 						
 						<div class="form-group">
                             <center>
-								<div class="col-md-6 col-md-offset-4">
-									<button type="submit" class="btn btn-primary">
+								<button type="submit" class="btn btn-primary">
                                     Thêm
-									</button>
-								</div>
+								</button>
 							</center>
                         </div>
                     </form>
