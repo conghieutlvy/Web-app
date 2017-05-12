17d3b55558e73af1656024ed0d09dd968023fc6d
hoanganhit1997 <hoanganhit1997@gmail.com>
2017-05-12 16:07:33 +0700

    Sua home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,31 @@
 @extends('Template/header')
 @section('page')
 			<div class="row">
-				<div class="col-md-4">
+				<div class="col-md-6">
 					<div class="thumbnail">
 						<a class="btn btn-primary" href="doExam">Thêm câu hỏi
 							<img alt="Thêm câu hỏi" src="8.jpg">
 						</a>
 					</div>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-6">
 					<div class="thumbnail">
 						<a class="btn btn-primary" href="doExam">Sửa câu hỏi
 							<img alt="Sửa câu hỏi" src="9.jpg">
 						</a>
 					</div>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-6">
 					<div class="thumbnail">
 						<a class="btn btn-primary" href="doExam">Xóa câu hỏi
 							<img alt="Xóa câu hỏi" src="10.jpg">
+						</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="thumbnail">
+						<a class="btn btn-primary" href="doExam">Ý kiến phản hồi
+							<img alt="Ý kiến phản hồi" src="11.jpg">
 						</a>
 					</div>
 				</div>
