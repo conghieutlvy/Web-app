c59930617d4f72ae6efb8ecf44603b341ba394f7
conghieutlvy <conghieutlvy@gmail.com>
2017-05-15 00:16:10 +0700

    Cập nhật sửa lỗi icon

--- resources/views/chooseExam.blade.php
+++ resources/views/chooseExam.blade.php
@@ -5,27 +5,20 @@
     </div>
     <br />
     <br />
-@if (!Auth::guest())
-<?php $url = "../";
-?>
-@else
-<?php $url = "";
-?>
-@endif
     <div class="row">	
     	<center><a href="doExam/0"> 
-   			<img src="<?php echo $url."so1.jpg"?>" alt="" border="1"/> 
+   			<img src="{{url('so1.jpg')}}" alt="" border="1"/> 
 		</a>
         <a href="doExam/1"> 
-   			<img src="<?php echo $url."so2.jpg"?>" alt="" border="1"/> 
+   			<img src="{{url('so2.jpg')}}" alt="" border="1"/> 
 		</a></center>
      </div>
      <div class="row">	
     	<center><a href="doExam/2"> 
-   			<img src="<?php echo $url."so3.jpg"?>" alt="" border="1"/> 
+   			<img src="{{url('so3.jpg')}}" alt="" border="1"/> 
 		</a>
         <a href="doExam/3"> 
-   			<img src="<?php echo $url."so4.jpg"?>" alt="" border="1"/> 
+   			<img src="{{url('so4.jpg')}}" alt="" border="1"/> 
 		</a></center>
      </div>
     
