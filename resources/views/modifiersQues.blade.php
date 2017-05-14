e0c9c006c509f7c3dcb1adde13333b3ff6be5ae7
conghieutlvy <conghieutlvy@gmail.com>
2017-05-14 22:36:15 +0700

    Chức nang xóa câu hỏi

---
+++ resources/views/modifiersQues.blade.php
@@ -0,0 +1,117 @@
+@extends('Template.header')
+<!--{{ route('register') }}-->
+@section('page')
+<div class="container" style="margin-top: 40px;">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Sửa câu hỏi</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+
+                        
+						
+						<div class="form-group">
+                            <center>
+								<button id="btadd" type="submit" class="btn btn-primary">
+                                    Sửa
+								</button>
+							</center>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php 
+	function showques($ques){
+		?><div class="form-group">
+                            <label for="ques" class="col-md-3 control-label">Câu hỏi:</label>
+
+                            <div class="col-md-7">
+                                <input id="ques" type="text" class="form-control" name="ques" required autofocus value="<?php echo $ques['question']; ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="firt-answer" class="col-md-3 control-label">Đáp án 1:</label>
+					
+                            <div class="col-md-7">
+                                <input  id="answer1" type="second-answer" class="form-control" name="answer1" required value="<?php echo $ques['c0']; ?>">
+                            </div>
+							<div class="col-md-2">
+								<input id = "cb1" onclick="switchadd();" type="checkbox" name="a1" value="1" checked = "<?php echo $ques['a0']; ?>"/> 
+							</div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="second-answer" class="col-md-3 control-label">Đáp án 2:</label>
+	
+                            <div class="col-md-7">
+                                <input  id="answer2" type="second-answer" class="form-control" name="answer2" required value="<?php echo $ques['c1']; ?>">
+                            </div>
+							<div class="col-md-2">
+								<input id = "cb2" onclick="switchadd();" type="checkbox" name="a2" value="1" checked = "<?php echo $ques['a0']; ?>" /> 
+							</div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="third-answer" class="col-md-3 control-label">Đáp án 3:</label>
+			
+                            <div class="col-md-7">
+                                <input id="answer3" type="third-answer" class="form-control" name="answer3" value="<?php echo $ques['c2']; ?>">
+                            </div>
+							<div class="col-md-2">
+								<input id = "cb3" onclick="switchadd();" type="checkbox" name="a3" value="1" checked = "<?php echo $ques['a0']; ?>"/> 
+							</div>
+                        </div>
+						
+						<div class="form-group">
+                            <label for="fourth-answer" class="col-md-3 control-label">Đáp án 4:</label>
+			
+                            <div class="col-md-7">
+                                <input id="answer4" type="fourth-answer" class="form-control" name="answer4" value="<?php echo $ques['c3']; ?>">
+                            </div>
+							<div class="col-md-2">
+								<input id = "cb4" onclick="switchadd();" type="checkbox" name="a4" value="1" checked = "<?php echo $ques['a0']; ?>" /> 
+							</div>
+                        </div>
+						
+						<div class="form-group"> 
+							<label for="InputFile" class="col-md-3 control-label" >  
+								Thêm ảnh: 
+							</label>
+                            <div class="col-md-7">
+                                <input type="file" id="ifile" name="up_img" /> 
+                                <p class="help-block"> Chọn ảnh từ máy tính. 
+                                </p>
+                            </div> 
+						</div>
+<?php	
+	}
+?>
+<script language="javascript">
+	function switchadd() {
+		var answer1 = document.getElementById('answer1');
+		var answer2 = document.getElementById('answer2');
+		var answer3 = document.getElementById('answer3');
+		var answer4 = document.getElementById('answer4');
+		var cb1 = document.getElementById('cb1');
+		var cb2 = document.getElementById('cb2');
+		var cb3 = document.getElementById('cb3');
+		var cb4 = document.getElementById('cb4');
+		if((cb1.checked && answer1.value != "")
+		 ||(cb2.checked && answer2.value != "")
+		 ||(cb3.checked && answer3.value != "")
+		 ||(cb4.checked && answer4.value != "")){
+			document.getElementById("btadd").removeAttribute("disabled");
+		}
+		else{
+			document.getElementById("btadd").setAttribute("disabled","disabled");
+		}
+	}
+
+</script>
+@endsection
---
+++ resources/views/selectQues.blade.php
@@ -0,0 +1,66 @@
+@extends('Template/header')
+
+@section('page')
+
+<br /><br />
+<center><h2> Chọn câu hỏi cần <?php if($key == 0) echo "xóa";
+ else echo "sửa" ?></h2></center>
+<br /><br />
+<form name= "checkform" action="modifieresques/<?php echo $key ?>" method="POST" >
+<div class="row">
+    <center>
+    <table cellspacing="20px" cellpadding="20px" border="1" >
+        <tr>
+            <th width="60"><center>Chọn</center></th>
+            <th width="40"><center>ID</center></th>
+            <th width="800"><center>Câu hỏi</center></th>
+        </tr>
+    <?php
+        $url = '../picture/';	 
+        $i = 0;
+        foreach($data as $ques) {
+                 getques($i, $ques, $url); 
+                 $i++;       
+            }
+        foreach($dataimg as $ques){
+            getques($i, $ques, $url);
+            $i++;
+        }
+    ?>
+    </table>
+    </center>
+</div>
+<br>
+<div class = "row">
+<center>
+    <input width="300" id = "sumbit" type="submit" class="btn btn-lg btn-success btn-fixed-right" name="submit-form" value="<?php if($key == 0) echo "Xóa" ;
+	else echo "sửa" ?>" /></center>
+</div>
+</form> 
+
+	
+<?php
+	function getques($number,$ques,$url){
+		?><tr>
+		<th><center>
+			<input type="checkbox" name = "cb[]" id = "<?php echo $number ?>" value="<?php if(isset($ques["name_img"])) $str1 = "0" ; else $str1 = "1";
+			if($ques['id'] < 10) $str2 = "0".$ques['id']; else $str2 = $ques['id'];
+			echo $str1."_".$str2 ?>"  align="right"/></center>
+		</th>
+        <th><center> <?php
+			echo $ques['id'];
+		?></center></th>
+		<th><?php
+			echo $ques['question'];
+		if(isset($ques["name_img"])) { ?>
+            <br><center><img src="
+        <?php echo $url.$ques["name_img"]; ?>
+            " alt = ""/>
+            </center>
+        <?php }?>
+        </th>
+        </tr><?php
+	}
+?>
+
+@stop
