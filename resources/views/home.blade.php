38597ef033843a51116d409a90edae91de79b58b
hoanganhit1997 <hoanganhit1997@gmail.com>
2017-05-16 01:56:08 +0700

    Sửa home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,41 +1,61 @@
 @extends('Template/header')
 @section('page')
 			<div class="row">
-				<div class="col-md-6">
+				<div class="col-md-4">
 					<div class="thumbnail">
-						<a class="btn btn-primary" href="home/addQues">Thêm câu hỏi
-							<img alt="Thêm câu hỏi" src="{{url('8.jpg')}}">
-						</a>
+						<img alt="Đề ngẫu nhiên" src="8.jpg">
+						<div class="caption">
+							<h3>
+								Thêm câu hỏi
+							</h3>
+							<a class="btn btn-primary" href="home/addQues">Thêm</a>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-6">
+				<div class="col-md-4">
 					<div class="thumbnail">
-						<a class="btn btn-primary" href="home/selectques">Sửa câu hỏi
-							<img alt="Sửa câu hỏi" src="{{url('9.jpg')}}">
-						</a>
+						<img alt="Đề ngẫu nhiên" src="9.jpg">
+						<div class="caption">
+							<h3>
+								Sửa câu hỏi
+							</h3>
+							<a class="btn btn-primary" href="home/selectques">Sửa</a>
+						</div>
 					</div>
 				</div>
 				@if(Auth::user()->level != 0)
-                <div class="col-md-6">
+                <div class="col-md-4">
 					<div class="thumbnail">
-						<a class="btn btn-primary" href="home/addadmin">Thêm admin
-							<img alt="" src="{{url('12.jpg')}}">
-						</a>
+						<img alt="Đề ngẫu nhiên" src="10.jpg">
+						<div class="caption">
+							<h3>
+								Thêm admin
+							</h3>
+							<a class="btn btn-primary" href="home/addQues">Thêm</a>
+						</div>
 					</div>
 				</div>
-                <div class="col-md-6">
+                <div class="col-md-4">
 					<div class="thumbnail">
-						<a class="btn btn-primary" href="home/selectadmin">Sửa admin
-							<img alt="" src="{{url('13.jpg')}}">
-						</a>
+						<img alt="Đề ngẫu nhiên" src="11.jpg">
+						<div class="caption">
+							<h3>
+								Sửa admin
+							</h3>
+							<a class="btn btn-primary" href="home/addQues">Sửa</a>
+						</div>
 					</div>
 				</div>
                 @endif
-                <div class="col-md-6">
+                <div class="col-md-4">
 					<div class="thumbnail">
-						<a class="btn btn-primary" href="home/feedback">Ý kiến phản hồi
-							<img alt="Ý kiến phản hồi" src="{{url('11.jpg')}}">
-						</a>
+						<img alt="Đề ngẫu nhiên" src="12.jpg">
+						<div class="caption">
+							<h3>
+								Ý kiến phản hồi
+							</h3>
+							<a class="btn btn-primary" href="home/addQues">Xem</a>
+						</div>
 					</div>
 				</div>
 			</div>
