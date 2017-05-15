aa05d117c205c270ca7728422630c728ad89d46a
conghieutlvy <conghieutlvy@gmail.com>
2017-05-16 00:49:11 +0700

    sửa csdl

--- database/migrations/2017_05_11_210146_create_ques_imgs_table.php
+++ database/migrations/2017_05_11_210146_create_ques_imgs_table.php
@@ -16,7 +16,7 @@ class CreateQuesImgsTable extends Migration
         Schema::create('ques_imgs', function (Blueprint $table) {
             $table->increments('id')->unique();
 			$table->text('question');
-			$table->string('namg_img');
+			$table->string('name_img');
 			$table->text('c0');
 			$table->text('c1');
 			$table->text('c2')->nullable();
@@ -25,6 +25,7 @@ class CreateQuesImgsTable extends Migration
 			$table->boolean('a1')->default(0);
 			$table->boolean('a2')->nullable();
 			$table->boolean('a3')->nullable();
+			$table->integer('user_id');
             $table->timestamps();
         });
     }
